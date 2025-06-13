0a663b820302578eca342e4085e0cbcb182c3bb5
Andrew D. <159858518+Agaumon@users.noreply.github.com>
2025-06-13 22:11:11 +0000

    Adding all files to repository

---
+++ main.py
@@ -0,0 +1,13 @@
+print("MY LOGIN SYSTEM")
+print("================")
+username = input("Username > ")
+password = input("Password > ")
+
+if username == "agaumon":
+  print("Welcome agaumon, your dashboard is ready.")
+elif username == "mom":
+  print("Go away mom, this isn't your computer.")
+elif username == "Noobmaster69":
+  print("I've called Thor to this location, he's coming for you.")
+else:
+  print("You don't belong here. Leave now and do not try to login again.")
