b9e18c04c5b2398d3c4897796be34730ae47b73a
Andrew D. <159858518+Agaumon@users.noreply.github.com>
2025-06-13 22:01:29 +0000

    Adding all Day 5 files

---
+++ main.py
@@ -0,0 +1,20 @@
+print("""Homestuck Character Creator
+--""")
+glasses = input("Do you wear glasses? ")
+if glasses == "yes":
+  print("Your character is John Egbert")
+  exit()
+pet = input("Do you have a pet? ")
+if pet == "yes":
+  print("Your character is Jade Harley")
+  exit()
+goth = input("Are you goth? ")
+if goth == "yes":
+  print("Your character is Rose Lalonde")
+  exit()
+cool = input("Are you a cool guy? ")
+if cool == "yes":
+  print("Your character is Dave Strider")
+  exit()
+else :
+  print("You're not cool enough to be a Homestuck character")
