5200c5e9a1e20343a4234f448f4fa2e043d072ee
Andrew D. <159858518+Agaumon@users.noreply.github.com>
2025-06-13 21:53:09 +0000

    Adding all files to repository

---
+++ main.py
@@ -0,0 +1,32 @@
+print("=== Welcome to a Love Story ===")
+print("Let's start off with your name.")
+yourName = input("What is your name? ")
+print("Hello,", yourName ,". Next let's start off to what brought you here.")
+yourReason = input("What brought you here today? ")
+print("Well, ", yourName,"I'm glad you stopped by for", yourReason , "." )
+print("""Today we'll be creating a love story for you.
+A love story that will fill you with joy, hope, sadness, and 
+despair. A tragedy.
+Like most love stories, there is always love, but it always ends in tragedy.
+A broken heart or two here and a happy ending there. No story is perfect.
+But that's what makes them so interesting. So let's begin.""")
+aLove = input("We can't use real names, so what is the name of your love? ")
+connectEd = input ("What magical or non magical thing connected you to " + aLove + "? ")
+yourOrigin = input("Where did you meet " + aLove + "? ")
+print("You.")
+print(connectEd ,"laid you you in front of me at ",yourOrigin ,".")
+print("I knew I was already rewriting my story for you. ")
+print(" Then—gone. A gust of wind, a closing door, no name, no trace. ")
+print("Fate’s cruel little tease. ")
+
+print(" But the universe—it’s not random, is it? Because hours later, there you are. ")
+print("Different place, same you. Coffee in hand, smile like sunrise, and this time… ")
+print("you see me.")
+
+print(" We talk. Just a moment. A laugh. A spark. And it’s not just chemistry—it’s code.")
+print(" A perfect loop, seamless. No bugs, no errors, just...us. ")
+
+print(aLove, ", the story I didn’t know I was searching for.")
+
+print(" And now?")
+print(" Now I can’t imagine writing the next chapter without you.")
