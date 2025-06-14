acaa094b6e215ca2305526c24e1d79fee4a9c5f7
Andrew D. <159858518+Agaumon@users.noreply.github.com>
2025-06-14 19:34:33 +0000

    Updated main.py

--- Day_11/main.py
+++ Day_11/main.py
@@ -3,8 +3,8 @@ years = int(input("How many years? "))
 months = years * 12
 print("There are",months,"months in",years,"years.")
 days = years * 365
-if years / 4 == 1:
-  days = days + 1
+leapDays = years // 4
+days = days + leapDays
 print("There are",days,"days in",months,"months.")
 hours = days * 24
 print("There are",hours,"hours in",days,"days.")
