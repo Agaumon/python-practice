91196b9f64e6015fddadeb9ea4ee90bfafd7e7aa
Andrew D. <159858518+Agaumon@users.noreply.github.com>
2025-06-21 20:57:51 +0000

    Adding all files to Day 18

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ count = 0
 while True:
   answer = int(input("What is your guess? "))
   if answer != number:
-    if answer < number:
+    if answer < number and answer > 0:
       print("Number is too low. Try again")
       count += 1
     elif answer > number:
       print("Number is too high. Try again.")
       count += 1
+    elif answer < 0:
+      exit()
   elif answer == number:
     count += 1
     print("Good job! You got it right! You got it in",count,"tries.")
-    exit()
-  elif answer < 0:
     exit()
