52dc626bbff0eb4e6969f3ef28c95930cd55de4e
Andrew D. <159858518+Agaumon@users.noreply.github.com>
2025-06-29 23:40:48 +0000

    Adding files for Day 26

--- main.py
+++ main.py
@@ -11,18 +11,24 @@ def pause():
   pygame.mixer.pause()
 
 pause()
+def unpause():
+  pygame.mixer.unpause()
+
 def play():
   # Play the sound
   pygame.mixer.unpause()
   while True:
     # Start taking user input and doing something with it
     os.system("clear")
-    option = input("""Press P to Pause
-    Press M for main menu """)
+    option = input("""
+    Press P to Pause
+    Press M for main menu
+    """)
     if option.upper() == "P":
       pause()
-      break
+      continue
     elif option.upper() == "M":
+      pause()
       break
     else:
       print("Please enter a valid response.")
@@ -33,18 +39,17 @@ while True:
   # Show the menu
   print("Audio Player Main Menu")
   print("Press 1 to Play")
-  print("Press 2 to Exit and Quit")
-  main_option = int(input("Please select one of the options above: "))
+  print("Press 2 to Exit")
+  main_option = input("Please select one of the options above: ")
   # take user's input
-  if main_option == 1:
+  if main_option == "1":
     play()
-  elif main_option == 2:
+  elif main_option == "2":
     print("Goodbye!")
     pygame.quit()
     exit()
   else:
     print("Please enter a valid response.")
+    time.sleep(1)
     continue
   # check whether you should call the play() subroutine depending on user's input
-  if True:
-    play()
